c77f73eed9a62d4748f83099e20965d0c14fb441
frikal-ahmed <ahmedfrikal50@gmail.com>
2021-01-16 20:18:02 +0100

    Page_aceuil

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aceuil</title>
+</head>
+<body>
+    <div class="container">
+        <div class="Utilisateur" >
+           <h4 id="textut"> La liste Des Utilisateur </h4>
+       <a href="Connecter.php"><img src="Myphpto.png" alt="UTilisateur" ></a>
+        </div>
+    <video class="video_entente" controls  >
+      <source src="avance-seul-la-meilleure-video-de-motivation.mp4"" type="video/mp4" >
+       </video>
+    </div>
+    <div class="speach">
+    <h1>Bienvenue Sur Application</h1>
+    <p><span>Application qui permet de gereé Votre Temps</span>
+    Et Organise Votre Temps et Avoir les objectifes De Votre Jour 
+    </p>
+    </div>
+    <div style="text-align:center;padding:1em 0;"> 
+    <h2>
+        <a style="text-decoration:none;" href="https://www.zeitverschiebung.net/fr/city/2540483">
+        <span style="color:sandybrown;">Heure actuelle</span><br />Oujda-Angad, Maroc</a>
+    </h2>
+     <iframe src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=fr&size=large&timezone=Africa%2FCasablanca&show=hour_minute" width="100%" height="140" frameborder="0" seamless>
+    </iframe> 
+    </div>
+    <div class="button-section">
+    <a href="conecter.php" class="button">Contacer Nous </a>
+    </div>
+</body>
+</html>
